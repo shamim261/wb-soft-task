edad1333614d7301e3735879ea9c407ed0ef53c7
Shamim Reza <mdshamimreza5552@gmail.com>
2024-10-22 14:27:01 +0600

    add icon to topnavbar

--- src/Shared/Navbar/NavbarTop.jsx
+++ src/Shared/Navbar/NavbarTop.jsx
@@ -1,38 +1,40 @@
-import { MdMenu } from "react-icons/md";
+import { ShoppingCartIcon } from "lucide-react";
 import { useContext, useEffect } from "react";
-import { OrderContext } from "../../ContextAPIs/OrderProvider";
-import { Link, useNavigate } from "react-router-dom";
-import useAxiosSecure from "../../Hooks/useAxiosSecure";
-import { toast } from "react-toastify";
-import useUser from "../../Security/useUser";
 import { FaUserCircle } from "react-icons/fa";
+import { MdMenu } from "react-icons/md";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useCart } from "../../ContextAPIs/CartContext";
+import { OrderContext } from "../../ContextAPIs/OrderProvider";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import useSmallScreen from "../../Hooks/useSmallScreen";
+import useUser from "../../Security/useUser";
 
 const NavbarTop = () => {
   const { open, setOpen, sidebarRef } = useContext(OrderContext);
+  const { state, dispatch } = useCart();
+  const cartItems = state.cart.cartItems;
   const [isSmallScreen] = useSmallScreen();
   const axiosSecure = useAxiosSecure();
   const navigate = useNavigate();
-  const [userData, ,refetch] = useUser();
-  const imgUrl = `https://littleaccount.com/uploads/userProfile/`
-
+  const [userData, , refetch] = useUser();
+  const imgUrl = `https://littleaccount.com/uploads/userProfile/`;
 
   const handleLogout = async () => {
     try {
-      const res = await axiosSecure('/api/logout')
-    if(res.data){
-      navigate('/login')
-      localStorage.removeItem('token')
-      toast.success('Logout Successfully')
-      window.location.reload();
-      refetch()
-    }
+      const res = await axiosSecure("/api/logout");
+      if (res.data) {
+        navigate("/login");
+        localStorage.removeItem("token");
+        toast.success("Logout Successfully");
+        window.location.reload();
+        refetch();
+      }
     } catch (err) {
-      toast.error(err.response.data.message)
+      toast.error(err.response.data.message);
     }
-  }
+  };
 
- 
   const handleClickOutside = (event) => {
     if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
       setOpen(false);
@@ -40,25 +42,25 @@ const NavbarTop = () => {
   };
 
   useEffect(() => {
-  if(isSmallScreen){
-    if (open) {
-      document.addEventListener('mousedown', handleClickOutside);
-    } else {
-      document.removeEventListener('mousedown', handleClickOutside);
+    if (isSmallScreen) {
+      if (open) {
+        document.addEventListener("mousedown", handleClickOutside);
+      } else {
+        document.removeEventListener("mousedown", handleClickOutside);
+      }
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
     }
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }
   }, [open, isSmallScreen]);
 
   useEffect(() => {
-    if(isSmallScreen){
-      setOpen(false)
-    } else{
-      setOpen(true)
+    if (isSmallScreen) {
+      setOpen(false);
+    } else {
+      setOpen(true);
     }
-  }, [isSmallScreen, setOpen])
+  }, [isSmallScreen, setOpen]);
 
   return (
     <div className="bg-white py-pt_primary text-_white w-full shadow-md border-b-1 ">
@@ -71,39 +73,56 @@ const NavbarTop = () => {
         </div>
         <div className="hidden lg:block"></div>
 
-        <div
-          className="flex flex-col items-center justify-center text-text_sm font-semibold relative group"
-        >
+        <div className="flex flex-col items-center justify-center text-text_sm font-semibold relative group">
           <div className="flex items-center gap-8">
-          <h1 className="text-blue-500 text-xl font-medium">{userData?.userData.name}</h1>
-         {userData?.userData.image ? 
-         <img
-            className="w-[40px] h-[40px] rounded-full"
-            src={`${imgUrl}${userData.userData.image}`}
-            alt=""
-          /> : 
-          <FaUserCircle className="w-[40px] h-[40px] rounded-full text-black" />}
+            <div
+              onClick={() => navigate("/cart")}
+              className="relative mr-10 cursor-pointer"
+            >
+              <ShoppingCartIcon className="w-6 h-6" color="black" />
+              <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+                {cartItems.length}
+              </span>
+            </div>
+
+            <h1 className="text-blue-500 text-xl font-medium">
+              {userData?.userData.name}
+            </h1>
+            {userData?.userData.image ? (
+              <img
+                className="w-[40px] h-[40px] rounded-full"
+                src={`${imgUrl}${userData.userData.image}`}
+                alt=""
+              />
+            ) : (
+              <FaUserCircle className="w-[40px] h-[40px] rounded-full text-black" />
+            )}
           </div>
 
           <div className="absolute top-10 right-3 bg-_white shadow-md rounded-sm overflow-hidden pt-2 w-48 z-10 group-hover:scale-100 transition-transform duration-300 transform origin-top-right scale-0">
-            {userData && <Link
-              to="/profile"
-              className="block px-4 py-2 text-black hover:bg-bg_selected hover:text-white"
-            >
-              Profile
-            </Link>}
-            {userData ? <Link
-            onClick={handleLogout}
-              className="block px-4 py-2 text-black hover:bg-bg_selected hover:text-white"
-            >
-              Logout
-            </Link> : 
-            <Link
-            to='/login'
-              className="block px-4 py-2 text-black hover:bg-bg_selected hover:text-white"
-            >
-              Login
-            </Link>}
+            {userData && (
+              <Link
+                to="/profile"
+                className="block px-4 py-2 text-black hover:bg-bg_selected hover:text-white"
+              >
+                Profile
+              </Link>
+            )}
+            {userData ? (
+              <Link
+                onClick={handleLogout}
+                className="block px-4 py-2 text-black hover:bg-bg_selected hover:text-white"
+              >
+                Logout
+              </Link>
+            ) : (
+              <Link
+                to="/login"
+                className="block px-4 py-2 text-black hover:bg-bg_selected hover:text-white"
+              >
+                Login
+              </Link>
+            )}
           </div>
         </div>
       </ul>
