6c76059ccbb1a3fb72bea5b9e88c007187fdb813
Shamim Reza <mdshamimreza5552@gmail.com>
2024-10-22 16:23:36 +0600

    checkout form partially done

--- src/Page/Cart/Cart.jsx
+++ src/Page/Cart/Cart.jsx
@@ -29,126 +29,130 @@ const Cart = () => {
       <h1 className="text-sm text-start md:text-text_xl lg:py-0 font-bold">
         Cart
       </h1>
-      <div className="pt-p_16px">
-        <div className="lg:flex items-start gap-3">
-          <div className="w-full lg:w-[58%] bg-white border-2">
-            <table className=" overflow-x-auto  w-full">
-              <thead>
-                <tr className="border-b-4 border-gray-300">
-                  <th className="text-[14.4px] w-6/12 font-bold p-[7px] text-black">
-                    Course
-                  </th>
-                  <th className="text-[14.4px] font-bold p-[7px] text-black">
-                    Price
-                  </th>
-                  <th className="text-[14.4px] font-bold p-[7px] text-black">
-                    Quantity
-                  </th>
-                  <th className="text-[14.4px] font-bold p-[7px] text-black">
-                    Sub Total
-                  </th>
-                </tr>
-              </thead>
+      {cartItems.length === 0 ? (
+        <h2 className="py-4">No items in the cart</h2>
+      ) : (
+        <div className="pt-p_16px">
+          <div className="lg:flex items-start gap-3">
+            <div className="w-full lg:w-[58%] bg-white border-2">
+              <table className=" overflow-x-auto  w-full">
+                <thead>
+                  <tr className="border-b-4 border-gray-300">
+                    <th className="text-[14.4px] w-6/12 font-bold p-[7px] text-black">
+                      Course
+                    </th>
+                    <th className="text-[14.4px] font-bold p-[7px] text-black">
+                      Price
+                    </th>
+                    <th className="text-[14.4px] font-bold p-[7px] text-black">
+                      Quantity
+                    </th>
+                    <th className="text-[14.4px] font-bold p-[7px] text-black">
+                      Sub Total
+                    </th>
+                  </tr>
+                </thead>
 
-              <tbody className="overflow-x-auto ">
-                {cartItems.map((item) => (
-                  <tr
-                    key={item.id}
-                    className="border-b border-gray-300 overflow-x-auto"
-                  >
-                    <td>
-                      <div className="flex items-center justify-center ">
-                        <div
-                          className="w-[20%] text-center flex items-center justify-center cursor-pointer"
-                          onClick={() => removeItem(item)}
-                        >
-                          <RiDeleteBin5Line className="text-xl hover:text-footer_color cursor-pointer" />
-                        </div>
-                        <div className="flex flex-col text-center justify-center items-center py-2  w-[80%]">
-                          <div className="mask">
-                            <img
-                              className="h-[40px] w-[70px] object-contain"
-                              src={item.photo}
-                              alt="Course"
-                            />
-                          </div>
-                          <p className="text-[14.4px] px-[7px] text-center flex ">
-                            {item.course_name}
-                          </p>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <p className="text-[14.4px] font-bold p-[7px] text-black text-center">
-                        {item.discount_price}
-                      </p>
-                    </td>
-                    <td>
-                      <div className="flex justify-center">
-                        <div className="border">
-                          <button
-                            disabled={item.quantity === 1}
-                            className="px-4 w-[30px] font-bold font_standard my-1.5"
-                            onClick={() =>
-                              updateQuantity(item, (item.quantity || 1) - 1)
-                            }
+                <tbody className="overflow-x-auto ">
+                  {cartItems.map((item) => (
+                    <tr
+                      key={item.id}
+                      className="border-b border-gray-300 overflow-x-auto"
+                    >
+                      <td>
+                        <div className="flex items-center justify-center ">
+                          <div
+                            className="w-[20%] text-center flex items-center justify-center cursor-pointer"
+                            onClick={() => removeItem(item)}
                           >
-                            -
-                          </button>
+                            <RiDeleteBin5Line className="text-xl hover:text-footer_color cursor-pointer" />
+                          </div>
+                          <div className="flex flex-col text-center justify-center items-center py-2  w-[80%]">
+                            <div className="mask">
+                              <img
+                                className="h-[40px] w-[70px] object-contain"
+                                src={item.photo}
+                                alt="Course"
+                              />
+                            </div>
+                            <p className="text-[14.4px] px-[7px] text-center flex ">
+                              {item.course_name}
+                            </p>
+                          </div>
                         </div>
-                        <div className="border-y">
-                          {/* <input
+                      </td>
+                      <td>
+                        <p className="text-[14.4px] font-bold p-[7px] text-black text-center">
+                          {item.discount_price}
+                        </p>
+                      </td>
+                      <td>
+                        <div className="flex justify-center">
+                          <div className="border">
+                            <button
+                              disabled={item.quantity === 1}
+                              className="px-4 w-[30px] font-bold font_standard my-1.5"
+                              onClick={() =>
+                                updateQuantity(item, (item.quantity || 1) - 1)
+                              }
+                            >
+                              -
+                            </button>
+                          </div>
+                          <div className="border-y">
+                            {/* <input
                             type="number"
                             className="font-bold w-[30px] lg:w-[60px] font_standard px-2 text-center mx-auto h-full"
                             value={item.quantity}
                           /> */}
-                          <span className="font-bold w-[30px] lg:w-[60px] font_standard px-2 text-center mx-auto h-full flex items-center justify-center">
-                            {item.quantity || 1}
-                          </span>
+                            <span className="font-bold w-[30px] lg:w-[60px] font_standard px-2 text-center mx-auto h-full flex items-center justify-center">
+                              {item.quantity || 1}
+                            </span>
+                          </div>
+                          <div className="border">
+                            <button
+                              onClick={() =>
+                                updateQuantity(item, (item.quantity || 1) + 1)
+                              }
+                              className="px-4 w-[30px] font-bold font_standard my-1.5"
+                            >
+                              +
+                            </button>
+                          </div>
                         </div>
-                        <div className="border">
-                          <button
-                            onClick={() =>
-                              updateQuantity(item, (item.quantity || 1) + 1)
-                            }
-                            className="px-4 w-[30px] font-bold font_standard my-1.5"
-                          >
-                            +
-                          </button>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <p className="text-[14.4px] font-bold p-[7px] text-black text-center">
-                        {(item.discount_price * item.quantity).toFixed(2)}
-                      </p>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <div className="lg:w-[41%] bg-white border-2 ">
-            <div className="px-[30px]">
-              <h2 className="font-bold text-start text-text_medium pt-2 pb-1 border-b-2 border-black">
-                Cart Summary
-              </h2>
-              <div className="py-3 flex justify-between border-b border-gray-300">
-                <p className="text-black font-bold">Total Price</p>
-                <p className="text-black font-bold">{totalPrice}</p>
-              </div>
+                      </td>
+                      <td>
+                        <p className="text-[14.4px] font-bold p-[7px] text-black text-center">
+                          {(item.discount_price * item.quantity).toFixed(2)}
+                        </p>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <div className="lg:w-[41%] bg-white border-2 ">
+              <div className="px-[30px]">
+                <h2 className="font-bold text-start text-text_medium pt-2 pb-1 border-b-2 border-black">
+                  Cart Summary
+                </h2>
+                <div className="py-3 flex justify-between border-b border-gray-300">
+                  <p className="text-black font-bold">Total Price</p>
+                  <p className="text-black font-bold">{totalPrice}</p>
+                </div>
 
-              <Link
-                to={`/cart/checkout`}
-                state={"bdt"}
-                className="font-medium text-black mb-2 border-2 hover:bg-[#D2C5A2] duration-300 py-2 px-4  block text-center mx-auto w-full"
-              >
-                PROCEED TO CHECKOUT
-              </Link>
+                <Link
+                  to={`/checkout`}
+                  state={"bdt"}
+                  className="font-medium text-black mb-2 border-2 hover:bg-[#D2C5A2] duration-300 py-2 px-4  block text-center mx-auto w-full"
+                >
+                  PROCEED TO CHECKOUT
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
